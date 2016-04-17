c1f12881128f8e497fbd522e8dc67702956032d9
MaDOS <rbechtel.ostmann@googlemail.com>
2016-04-17 23:51:07 +0200

    init with some basic stuff

---
+++ Examples/ExampleSettings.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using usefullStuff.Settings;
+using usefullStuff.Serialization;
+
+namespace usefullStuff.Examples
+{
+    [Serializable]
+    public class ExampleSettings
+    {
+        int data;
+        string moreData;
+
+        public int Data
+        {
+            get
+            {
+                return data;
+            }
+
+            set
+            {
+                data = value;
+            }
+        }
+
+        public string MoreData
+        {
+            get
+            {
+                return moreData;
+            }
+
+            set
+            {
+                moreData = value;
+            }
+        }
+    }
+
+    public class ExampleSettingsManager : SettingsManager
+    {
+        protected override event SavingInitiatedEventHandler SavingInitiated;
+        protected override event SavingDoneEventHandler SavingDone;
+        protected override event LoadingInitiatedEventHandler LoadingInitiated;
+        protected override event LoadingDoneEventHandler LoadingDone;
+        
+        private ExampleSettings settings;
+
+        public ExampleSettings Settings
+        {
+            get
+            {
+                return settings;
+            }
+
+            set
+            {
+                settings = value;
+            }
+        }
+
+        public ExampleSettingsManager()
+        { }
+
+        public override void Load(FileInfo file)
+        {
+            FileInfo dummyFileInfo = new FileInfo("C:\blub.bin");
+            
+            LoadingInitiated(new LoadingEventArgs() { File = dummyFileInfo });
+
+            this.Settings = Serializer.DeserializeFromBinary<ExampleSettings>(dummyFileInfo);
+
+            LoadingDone(new LoadingEventArgs() { File = dummyFileInfo });
+        }
+
+        public override void Save(FileInfo file)
+        {
+            FileInfo dummyFileInfo = new FileInfo("C:\blub.bin");
+
+            SavingInitiated(new SavingEventArgs() { File = dummyFileInfo });
+
+            Serializer.SerializeAsBinary<ExampleSettings>(this.settings, dummyFileInfo);
+
+            SavingDone(new SavingEventArgs() { File = dummyFileInfo });
+        }
+    }
+}
---
+++ Settings/Settings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usefullStuff.Settings
+{
+    public abstract class SettingsManager
+    {
+        public delegate void SavingInitiatedEventHandler(SavingEventArgs e);
+        public delegate void SavingDoneEventHandler(SavingEventArgs e);
+
+        public delegate void LoadingInitiatedEventHandler(LoadingEventArgs e);
+        public delegate void LoadingDoneEventHandler(LoadingEventArgs e);
+
+        protected abstract event SavingInitiatedEventHandler SavingInitiated;
+        protected abstract event SavingDoneEventHandler SavingDone;
+        protected abstract event LoadingInitiatedEventHandler LoadingInitiated;
+        protected abstract event LoadingDoneEventHandler LoadingDone;
+
+        public abstract void Save(FileInfo file);
+
+        public abstract void Load(FileInfo file);
+    }
+
+    public class LoadingEventArgs : EventArgs
+    {
+        FileInfo file;
+
+        public FileInfo File
+        {
+            get
+            {
+                return file;
+            }
+
+            set
+            {
+                file = value;
+            }
+        }
+    }
+
+    public class SavingEventArgs : EventArgs
+    {
+        FileInfo file;
+
+        public FileInfo File
+        {
+            get
+            {
+                return file;
+            }
+
+            set
+            {
+                file = value;
+            }
+        }
+    }
+}
